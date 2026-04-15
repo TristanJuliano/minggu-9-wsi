b6e9611ff78e512032451736d78cc52209f9e9de
fajarrama1210 <jarrr1210@gmail.com>
2026-04-16 02:50:40 +0700

    feat: add footer component with copyright notice and bootstrap script inclusion

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-        <p class="mb-0">&copy; <?php echo date("Y"); ?> Sistem Informasi Mahasiswa. BY Djarr.</p>
+        <p class="mb-0">&copy; <?php echo date("Y"); ?> Sistem Informasi Mahasiswa. BY Bolo Tuhan.</p>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
